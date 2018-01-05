1c577e75fb333744fc5023b1e16b691c2e450dea
ddmilo <danmilo.dev@gmail.com>
2018-01-05 02:30:38 -0500

    ITS WORKING!

--- giphee.js
+++ giphee.js
@@ -1,15 +1,30 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+const GphApiClient  = require('giphy-js-sdk-core');
+
+giphy = GphApiClient(config.giphy_token)
 
 client.on("ready", () => {
   console.log("I am ready!");
 });
 
 client.on("message", (message) => {
+  
+
   if(message.content.startsWith(config.prefix)) {
-    message.channel.send("pong!");
+    let tag = message.content.slice(config.prefix.length).trim().split(/ +/g)
+    if(tag == "help"){
+      message.channel.send("Type '!g' followed by a <tag>. ");      
+    }
+    else    
+      giphy.random('gifs', {tag})
+        .then((res) => {
+          console.log(res);
+          message.channel.send(res.data.url);        
+        })
   }
+  
 });
 
 client.login(config.token);
