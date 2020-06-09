06c8c86c7757b00d6733ae645223d8b93d71897c
Johan Lindskogen <johan.lindskogen@gmail.com>
2020-06-09 10:48:50 +0200

    Change giphy endpoint

--- giphee.js
+++ giphee.js
@@ -1,14 +1,17 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
-const GphApiClient  = require('giphy-js-sdk-core');
-const http = require('http');
-const express = require('express');
+const GphApiClient = require("giphy-js-sdk-core");
+const express = require("express");
 const app = express();
 
-require('dotenv').config();
+require("dotenv").config();
 
-giphy = GphApiClient(process.env.GIPHY_TOKEN)
+const giphy = GphApiClient(process.env.GIPHY_TOKEN);
+
+const searchGifs = (query) => {
+  return giphy.translate("gifs", { s: query });
+};
 
 app.get("/", (request, response) => {
   console.log(Date.now() + " Ping Received");
@@ -22,22 +25,21 @@ client.on("ready", () => {
 });
 
 client.on("message", (message) => {
-  
-
-  if(message.content.startsWith(config.prefix)) {
-    let tag = message.content.slice(config.prefix.length).trim().split(/ +/g)
-    if(tag == "help" || tag == ''){
-      message.channel.send("Type '!g' followed by a <tag>. Multiple tags must be separated by a '-'. ");      
+  if (message.content.startsWith(config.prefix)) {
+    let tag = message.content.slice(config.prefix.length).trim();
+    if (tag == "help" || tag == "") {
+      message.channel.send(
+        "Type '!g' followed by a <tag>. Multiple tags must be separated by space. "
+      );
+    } else {
+      searchGifs(tag).then((res) => {
+        // console.log(res)
+        message.channel.send(res.data.url);
+      });
     }
-    else    
-      giphy.random('gifs', {tag})
-        .then((res) => {
-          // console.log(res);
-          message.channel.send(res.data.url);        
-        })
   }
-  
 });
 
 client.login(process.env.DISCORD_TOKEN);
 
+// searchGifs("awesome").then(console.log);
